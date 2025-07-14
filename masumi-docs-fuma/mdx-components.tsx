09dc35a2e9a516eece85ae0716adad10050031b1
Albina <albina.nikiforova.5670@gmail.com>
2025-07-14 18:47:15 +0200

    nits

--- masumi-docs-fuma/mdx-components.tsx
+++ masumi-docs-fuma/mdx-components.tsx
@@ -6,11 +6,13 @@ import { APIPage } from 'fumadocs-openapi/ui';
 import { openapi } from '@/lib/source';
 import type { MDXComponents } from 'mdx/types';
 import {Mermaid}  from '@/components/mermaid';
+import * as icons from "lucide-react"
 
 // use this function to get MDX components, you will need it for rendering MDX
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...(icons as unknown as MDXComponents),
     img: (props) => <ImageZoom {...(props as any)} />,
     APIPage: (props) => <APIPage {...openapi.getAPIPageProps(props)} />,
     ImageCard,
