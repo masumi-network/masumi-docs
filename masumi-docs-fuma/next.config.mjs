dafd13851df863025193e7fd1db162856b592e23
Albina <albina.nikiforova.5670@gmail.com>
2025-07-17 16:54:15 +0200

    postential fix for a caching problem

--- masumi-docs-fuma/next.config.mjs
+++ masumi-docs-fuma/next.config.mjs
@@ -5,6 +5,19 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: '/:path*', // all dynamic routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store', // prevents DO or any CDN from caching streamed HTML
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
